9d54412ff8619abd9dd183b51510098a576bd86a
林泓均 <linhongjun@linhongjundeAir.lan>
2022-08-29 20:53:36 +0800

    Updates

---
+++ precache-manifest.c5680de6d3200e6a34467e6679407234.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b7a7edf19c5f37a6d01",
+    "url": "/robofriends/static/css/main.1ad5eeb5.chunk.css"
+  },
+  {
+    "revision": "9b7a7edf19c5f37a6d01",
+    "url": "/robofriends/static/js/main.9821648b.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "45064240bc63e9b879ac",
+    "url": "/robofriends/static/css/2.8fb356e8.chunk.css"
+  },
+  {
+    "revision": "45064240bc63e9b879ac",
+    "url": "/robofriends/static/js/2.fd0ca411.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "787bc15e8e243b5644be88e7dfd5ad83",
+    "url": "/robofriends/index.html"
+  }
+];
